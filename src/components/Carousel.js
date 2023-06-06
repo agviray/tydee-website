4dd9f915168b8a761ae648e68b64582933d7c876
Allan Viray <allang.v.dev@gmail.com>
2023-06-05 19:17:48 -0500

    Add carousel control buttons

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
 import useWindowDimensions from './hooks/useWindowDimensions';
-import { StyledCarousel, StyledCarouselItems } from './styles/Carousel.styled';
+import {
+  StyledCarouselWrapper,
+  StyledCarousel,
+  StyledCarouselItems,
+  StyledControl,
+} from './styles/Carousel.styled';
 
 const Carousel = ({ images }) => {
   return (
-    <StyledCarousel>
-      <div className="carouselContent">
-        <StyledCarouselItems>
-          {images.map((image, index) => (
-            <div key={index} className="imageContainer">
-              {image.content()}
-            </div>
-          ))}
-        </StyledCarouselItems>
-      </div>
-    </StyledCarousel>
+    <StyledCarouselWrapper>
+      <StyledControl className="previous">
+        <span></span>
+      </StyledControl>
+      <StyledCarousel>
+        <div className="carouselContent">
+          <StyledCarouselItems>
+            {images.map((image, index) => (
+              <div key={index} className="imageContainer">
+                {image.content()}
+              </div>
+            ))}
+          </StyledCarouselItems>
+        </div>
+      </StyledCarousel>
+      <StyledControl className="next">
+        <span></span>
+      </StyledControl>
+    </StyledCarouselWrapper>
   );
 };
 
--- src/components/styles/Carousel.styled.js
+++ src/components/styles/Carousel.styled.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const StyledCarouselWrapper = styled.div`
+  position: relative;
+  max-width: 3000px;
+  margin: 0 auto;
+  padding: 0 100px;
+`;
+
 export const StyledCarousel = styled.div`
   position: relative;
   overflow: hidden;
@@ -21,6 +28,67 @@ export const StyledCarouselItems = styled.div`
       height: 368px;
       object-fit: cover;
       object-position: 50% 50%;
+    }
+  }
+`;
+
+export const StyledControl = styled.div`
+  position: absolute;
+  top: 0;
+  height: 100%;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  border-radius: 7px;
+  background-color: white;
+  transition: all 0.2s ease;
+
+  span {
+    display: inline-block;
+    width: 35px;
+    height: 35px;
+    border-width: 5px 5px 0 0;
+    border-style: solid;
+    border-color: #006aff;
+  }
+
+  &.previous {
+    left: 0;
+    -webkit-box-shadow: 3px 0px 10px 1px rgba(51, 51, 51, 0.59);
+    -moz-box-shadow: 3px 0px 10px 1px rgba(51, 51, 51, 0.59);
+    box-shadow: 3px 0px 10px 1px rgba(51, 51, 51, 0.59);
+    span {
+      transform: translateX(9px) rotate(-135deg);
+    }
+  }
+
+  &.next {
+    right: 0;
+    -webkit-box-shadow: -3px 0px 10px 1px rgba(51, 51, 51, 0.59);
+    -moz-box-shadow: -3px 0px 10px 1px rgba(51, 51, 51, 0.59);
+    box-shadow: -3px 0px 10px 1px rgba(51, 51, 51, 0.59);
+    span {
+      transform: translateX(-8px) rotate(45deg);
+    }
+  }
+
+  &:hover {
+    cursor: pointer;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    background-color: #006aff;
+
+    span {
+      border-color: white;
+    }
+
+    &.previous {
+      left: 3px;
+    }
+
+    &.next {
+      right: 3px;
     }
   }
 `;
